f206e3f0eb00734e1a1571a671e99841ab3779af
Simon Strom <simon.strom@gmail.com>
2013-10-07 15:00:52 +0200

    fix indentation

--- lab2/date.h
+++ lab2/date.h
@@ -18,35 +18,33 @@ class lab2::Date {
 
     int days_diff(Date);
   public:
-   Date(int m_per_year, int d_per_week, int y, int m, int d);
-   int year();
-   int month();
-   int day();
-   int week_day();
-   int days_per_week();
-   int days_this_month();
-   int months_per_year();
+    Date(int m_per_year, int d_per_week, int y, int m, int d);
+    int year();
+    int month();
+    int day();
+    int week_day();
+    int days_per_week();
+    int days_this_month();
+    int months_per_year();
 
-   void add_year();
-   void add_year(int);
+    void add_year();
+    void add_year(int);
 
-   void add_month();
-   void add_month(int);
+    void add_month();
+    void add_month(int);
 
-   // Operators
-   Date & operator++(); // Prefix
-   Date & operator--();
+    // Operators
+    Date & operator++(); // Prefix
+    Date & operator--();
 
-   Date & operator-=(int);
-   Date & operator+=(int);
+    Date & operator-=(int);
+    Date & operator+=(int);
 
-   // Comperators
-   bool operator==( Date );
+    // Comperators
+    bool operator==( Date );
 
-   // Diff operators
-   int operator-( Date );
-
-
+    // Diff operators
+    int operator-( Date );
 };
 
 #endif
