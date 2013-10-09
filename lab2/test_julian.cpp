3c3c179fefe6807e4e963f402904934bfa016014
Simon Strom <simon.strom@gmail.com>
2013-10-09 15:00:00 +0200

    more tests for basic julian

--- lab2/test_julian.cpp
+++ lab2/test_julian.cpp
@@ -5,9 +5,11 @@ class JulianTestSuite : public CxxTest::TestSuite
 {
 public:
 
-  void test_initializing( void )
+  void test_initializing_and_basic_inheritance( void )
   {
     lab2::Julian j(2013, 10, 2);
     TS_ASSERT_EQUALS(j.year(), 2013);
+    TS_ASSERT_EQUALS(j.month(), 10);
+    TS_ASSERT_EQUALS(j.day(), 2);
   }
 };
